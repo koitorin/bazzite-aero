83c0396cfce31b4a4172c93c59597b41304017e2
Rin Koito <mouichidouryuuseigun@gmail.com>
2025-04-13 13:49:19 -0400

    feat(add): Add konsole and aeroplasmatheme install script

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,14 +3,11 @@
 set -ouex pipefail
 
 ### Install packages
+dnf5 install -y konsole
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Install aerothemeplasma
+chmod +x 10-aero-theme.sh
+bash 10-aero-theme.sh
 
 # Use a COPR Example:
 #
@@ -20,5 +17,4 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# systemctl enable podman.socket
